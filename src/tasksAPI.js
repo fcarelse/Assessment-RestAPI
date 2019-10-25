b08ca9eaf50571ee039125dee5f955601c7495bd
Francis Carelse <francis@carelse.org>
2019-10-25 11:47:52 +0100

    New router per call

--- src/tasksAPI.js
+++ src/tasksAPI.js
@@ -1,27 +1,32 @@
 const express = require('express')
-const router = express.Router()
+const tasks = require('./tasksController.js');
 
 /**
  * Module returns a builder function.
  * This creates an express router using the provided midddleware.
  * 
+ * Express middleware function or multiple wrapped in one.
+ * Callback arguments: request, response, next, parameters
  * @param middleware callback
  */
-module.exports = function(middleware){
+module.exports = function(middleware, options){
+	// New router instance to be used with this middleware.
+	const router = express.Router()
+
 	// Create route
-	router.put('/:id', tasks.create(middleware));
+	router.put('/:id', tasks.create(middleware, options));
 
 	// Read route
-	router.get('/:id', tasks.read(middleware));
+	router.get('/:id', tasks.read(middleware, options));
 
 	// Update route
-	router.patch('/:id', tasks.update(middleware));
+	router.patch('/:id', tasks.update(middleware, options));
 
 	// Delete route
-	router.delete('/:id', tasks.delete(middleware));
+	router.delete('/:id', tasks.delete(middleware, options));
 
 	// List route
-	router.get('/', tasks.list(middleware));
+	router.get('/', tasks.list(middleware, options));
 
 	// Return the built router.
 	return router;
