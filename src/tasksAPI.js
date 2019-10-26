8ba79c5b786f05760133e0ce9babadebae6c14e2
Francis Carelse <francis@carelse.org>
2019-10-26 19:48:14 +0100

    Added read and update handlers

--- src/tasksAPI.js
+++ src/tasksAPI.js
@@ -1,7 +1,7 @@
 /**
  * Tasks API
  * Author: Francis Carelse
- * 
+ *
  * This module handles configurations
  */
 
@@ -9,42 +9,90 @@
  * Default database configuration
  */
 export const defaultDB = {
-	client: 'mysql',
-	connection: {
-		host: '127.0.0.1',
-		user: 'admin',
-		password: 'password',
-		database: 'tasksapi',
-		charset: 'utf8',
-	}
-}
+  client: "mysql",
+  connection: {
+    host: "127.0.0.1",
+    user: "admin",
+    password: "admin",
+    database: "tasksapi",
+    charset: "utf8"
+  }
+};
 
 /**
  * Default Options to be used by router or controller
  */
 export const defaultOptions = {
-	base: '/tasks',
-	configDB: defaultDB,
+  base: "/tasks",
+  configDB: defaultDB
 };
 
 /**
  * Default middleware to be used by router or controller
  */
-export const defaultMiddleware = []
+export const defaultMiddleware = [];
 
-export const taskStatuses = ['ToDo', 'Doing', 'Testing', 'Done', 'Cancelled', 'Deleted']
-export const taskFields = ['id', 'status', 'title', 'content']
+/**
+ * Task Statuses
+ * List of text names for statuses for Tasks.
+ * Statuses exist within each stage of task pipeline
+ */
+export const taskStatuses = ["ToDo", "Doing", "Done", "Cancelled"];
 
-export const taskSchema = async(knex) => {
-	const exists = await knex.schema.hasTable('tasks')
-	if(!exists){
-		await new Promise((resolve, reject)=>{
-			knex.schema.createTable('tasks', function(tasks) {
-				tasks.increments('id').primary()
-				tasks.string('status', 20)
-				tasks.string('title', 200)
-				tasks.text('content')
-			});
-		})
-	}
-}
+/**
+ * Task Stage
+ * List of text names for stages for task pipeline.
+ */
+export const taskStages = ["Design", "Develop", "Test", "Deploy"];
+
+/**
+ * Task Fields
+ * Property key is field name
+ * Property value is javascript data type
+ */
+export const taskFields = {
+  id: "number",
+  stage: "string",
+  status: "string",
+  title: "string",
+  content: "string"
+};
+
+/**
+ * Task Schema
+ * Verifys and builds table for tasks
+ *
+ * @param knex Knex type data access object
+ */
+export const taskSchema = async knex => {
+  // Check if table exists
+  const exists = await knex.schema.hasTable("tasks");
+
+  // Only if table does not exist then create it.
+  if (!exists)
+    await new Promise(resolve => {
+      knex.schema.createTable("tasks", function(tasks) {
+        tasks.increments("id").primary();
+        tasks.string("stage", 20);
+        tasks.string("status", 20);
+        tasks.string("title", 200);
+        tasks.text("content");
+        resolve(true);
+      });
+    });
+
+  // Verify each critical field exists.
+  await new Promise(resolve => {
+    knex.schema.table("tasks", function(tasks) {
+      (async () => {
+        //Check for each Column
+        if (!(await tasks.hasColumn("id"))) tasks.increments("id").primary();
+        if (!(await tasks.hasColumn("stage"))) tasks.string("stage", 20);
+        if (!(await tasks.hasColumn("status"))) tasks.string("status", 20);
+        if (!(await tasks.hasColumn("title"))) tasks.string("title", 200);
+        if (!(await tasks.hasColumn("content"))) tasks.text("content");
+        resolve(true);
+      })();
+    });
+  });
+};
--- src/tasksController.js
+++ src/tasksController.js
@@ -1,39 +1,104 @@
 /**
  * Tasks Controller
  * Author: Francis Carelse
- * 
+ *
  */
-import {defaultOptions, defaultMiddleware, defaultDB} from './tasksAPI.js'
-import * as Knex from 'kenx'
+import { defaultOptions, defaultDB, taskFields } from "./tasksAPI.js";
+import * as Knex from "kenx";
 
 /**
  * Tasks Controller factory method
  */
-export default function(middleware, options){
-	// If middleware not supplied then provide default.
-	middleware = middleware || defaultMiddleware;
+export default function(options) {
+  // If options not supplied then use default.
+  options = options || defaultOptions;
 
-	// If options not supplied then use default.
-	options = options || defaultOptions;
+  // If Data Access Object not supplied then use default.
+  options.configDB = options.configDB || defaultDB;
 
-	// If Data Access Object not supplied then use default.
-	options.configDB = options.configDB || defaultDB;
+  // Controller to be returned
+  const Ctrl = {};
 
-	// Controller to be returned
-	const Ctrl = {};
+  // Database Data Access Object
+  const knex = Knex(options.configDB);
 
-	// Database Data Access Object
-	const knex = Knex(options.configDB);
+  /**
+   * Handler for Create request
+   */
+  Ctrl.create = (request, reply) => async () => {
+    // Retrieve the input task fields
+    const inputTask = request.payload.task;
 
-	/**
-	 * Handler for Create request
-	 */
-	Ctrl.create = (request, reply)=>
-		new Promise((resolve, reject)=>{
-			// Retrieve the new task
-			const newTask = request.payload.task;
-			// Build a query and execute.
-			Knex.insert(newTask,['id'])
-		})
+    // Extract the standard field names
+    const fields = Object.keys(taskFields);
 
+    // Limit to standard fields
+    const newTask = {};
+    fields.forEach(field => {
+      newTask[field] = inputTask[field];
+    });
+
+    // remove id field in case something was put in there
+    delete newTask.id;
+
+    // Build a query and execute.
+    const id = await knex.insert(newTask, ["id"]);
+
+    // Return created task and return create success status code.
+    return reply({ id, ...newTask }).code(options.createSuccessCode || 201);
+  };
+
+  /**
+   * Handler for Read request
+   */
+  Ctrl.read = (request, reply) => async () => {
+    // Retrieve the id for the task to return
+    const id = request.query.id;
+
+    // Extract the standard field names
+    const fields = Object.keys(taskFields);
+
+    // Catch errors
+    try {
+      // Build a query and execute.
+      const task = await knex.select(fields).where({ id });
+
+      // If no error return the retrieved task and read success status code
+      return reply(task).code(options.readSuccessCode || 200);
+
+      // Catch any error
+    } catch (err) {
+      // If error return not found code
+      return reply({ error: 404, message: "Not found" }).code(404);
+    }
+  };
+
+  /**
+   * Handler for Update request
+   */
+  Ctrl.update = (request, reply) => async () => {
+    // Retrieve the input task fields
+    const inputTask = request.payload.task;
+
+    // Extract the standard field names
+    const fields = Object.keys(taskFields);
+
+    // Limit to standard fields
+    const updateTask = {};
+    fields.forEach(field => {
+      updateTask[field] = inputTask[field];
+    });
+
+    // Extract id
+    const id = updateTask.id;
+
+    // Remove id from fields to update
+    delete updateTask.id;
+
+    // Build a query and execute.
+    const id = await knex.where("id", "=", id).update(updateTask);
+
+    // Return created task and return update success status code.
+    return reply(updateTask).code(options.updateSuccessCode || 204);
+  };
 }
