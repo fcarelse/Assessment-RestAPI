44adeb1d96e1d352b8cf78c5901d7fe3e28be9a9
Francis Carelse <francis@carelse.org>
2019-10-26 15:37:59 +0100

    Adding knex with basic schema

--- src/tasksAPI.js
+++ src/tasksAPI.js
@@ -2,21 +2,49 @@
  * Tasks API
  * Author: Francis Carelse
  * 
+ * This module handles configurations
  */
 
-import router from './tasksRouter.js'
-import controller from './tasksController.js'
+/**
+ * Default database configuration
+ */
+export const defaultDB = {
+	client: 'mysql',
+	connection: {
+		host: '127.0.0.1',
+		user: 'admin',
+		password: 'password',
+		database: 'tasksapi',
+		charset: 'utf8',
+	}
+}
 
 /**
  * Default Options to be used by router or controller
  */
 export const defaultOptions = {
 	base: '/tasks',
+	configDB: defaultDB,
 };
 
 /**
  * Default middleware to be used by router or controller
  */
-export const defaultMiddleware = [];
+export const defaultMiddleware = []
 
-export default {router, controller};
+export const taskStatuses = ['ToDo', 'Doing', 'Testing', 'Done', 'Cancelled', 'Deleted']
+export const taskFields = ['id', 'status', 'title', 'content']
+
+export const taskSchema = async(knex) => {
+	const exists = await knex.schema.hasTable('tasks')
+	if(!exists){
+		await new Promise((resolve, reject)=>{
+			knex.schema.createTable('tasks', function(tasks) {
+				tasks.increments('id').primary()
+				tasks.string('status', 20)
+				tasks.string('title', 200)
+				tasks.text('content')
+			});
+		})
+	}
+}
--- src/tasksController.js
+++ src/tasksController.js
@@ -3,7 +3,8 @@
  * Author: Francis Carelse
  * 
  */
-
+import {defaultOptions, defaultMiddleware, defaultDB} from './tasksAPI.js'
+import * as Knex from 'kenx'
 
 /**
  * Tasks Controller factory method
@@ -15,14 +16,24 @@ export default function(middleware, options){
 	// If options not supplied then use default.
 	options = options || defaultOptions;
 
+	// If Data Access Object not supplied then use default.
+	options.configDB = options.configDB || defaultDB;
+
 	// Controller to be returned
 	const Ctrl = {};
 
+	// Database Data Access Object
+	const knex = Knex(options.configDB);
+
 	/**
-	 * Controller for Create request
+	 * Handler for Create request
 	 */
-	Ctrl.create = (request, reply)=>new Promise((resolve, reject)=>{
-		request.payload.task
-	})
+	Ctrl.create = (request, reply)=>
+		new Promise((resolve, reject)=>{
+			// Retrieve the new task
+			const newTask = request.payload.task;
+			// Build a query and execute.
+			Knex.insert(newTask,['id'])
+		})
 
 }
